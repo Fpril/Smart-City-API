17b0101dbf6dc73c2f2c0e529045e96024c262b6
Fpril <fewpril@gmail.com>
2020-12-19 17:35:00 +0200

    add order model, controller, dao, routes

--- app.js
+++ app.js
@@ -13,6 +13,10 @@ var router = express.Router();
 app.use('/car', router);
 require('./Routes/car.routes')(router);
 
+var router = express.Router();
+app.use('/order', router);
+require('./Routes/order.routes')(router);
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`Server is running on ${process.env.PORT || PORT} port.`);
 });
